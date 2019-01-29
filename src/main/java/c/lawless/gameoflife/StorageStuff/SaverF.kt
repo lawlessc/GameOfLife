3356f04e4b3a45b9b8230ee63aa4c61fd2964d6a
Christopher Lawless <lawless85@gmail.com>
2019-01-29 00:23:44 +0000

    updated android studio, reimported project, add loader

--- src/main/java/c/lawless/gameoflife/StorageStuff/SaverF.kt
+++ src/main/java/c/lawless/gameoflife/StorageStuff/SaverF.kt
@@ -50,3 +50,41 @@ fun frameSaver(fb :FrameBuffer ,world: World)
 
 }
 
+
+
+
+fun frameLoader(fb :FrameBuffer ,world: World)
+{
+
+
+
+    //We render to the save texture here
+    fb.setRenderTarget(TextureManager.getInstance().getTexture(TextureNames.savetexture))
+    world.renderScene(fb)
+    world.draw(fb)
+    fb.display()
+    fb.removeRenderTarget()
+
+    //retrieve rendered to texture
+    var texture_to_save=  TextureManager.getInstance().getTexture(TextureNames.savetexture)
+
+    //create a bitmap of the image
+    var bitmap = Bitmap.createBitmap(texture_to_save.width, texture_to_save.height, Bitmap.Config.ARGB_8888)
+    var vec= fb.getPixels()
+    bitmap.copyPixelsFromBuffer(IntBuffer.wrap(vec));
+
+
+    //Convert the bitmap to a byte array
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+    val byteArray = stream.toByteArray()
+    bitmap.recycle()
+
+
+    //Save the byte array to a boxstore , useing current date time as a filename.
+    val box = boxStore.boxFor<GOFSave>()
+    box.put(GOFSave(0, java.util.Calendar.getInstance().toString(), GridSizes.size_level, byteArray))
+
+
+}
+
